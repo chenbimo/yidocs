b0913473a3b5859afbf137fd6ea2eb160e2f9209
陈随易 <chenbimo@gmail.com>
2025-02-10 17:33:50 +0800

    导入首页自定义文件

--- packages/yidocs/.vitepress/theme/index.js
+++ packages/yidocs/.vitepress/theme/index.js
@@ -1,6 +1,9 @@
 import DefaultTheme from 'vitepress/theme';
+import homeMore from './components/homeMore.vue';
 import './style/custom.css';
 export default {
     extends: DefaultTheme,
-    enhanceApp({ app, router, siteData }) {}
+    enhanceApp({ app, router, siteData }) {
+        app.component('homeMore', homeMore);
+    }
 };
